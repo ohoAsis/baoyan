6d9c4b44c7a2b525a4f314a1491f58459e46233c
ohoasis <851691158@qq.com>
2025-11-15 00:59:57 +0800

    优化

--- frontend_backup/src/api/index.ts
+++
@@ -1,5 +0,0 @@
-// 导出所有API
-export { authApi } from './auth';
-export { studentsApi } from './students';
-export { applicationsApi } from './applications';
-export { default as apiClient } from './client';
--- frontend_backup/src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-// 导出所有store
-export { useAuth, getCurrentUser } from './auth';
-export { useApplications } from './applications';
-export { useStudents } from './students';
